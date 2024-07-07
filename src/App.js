5e6f0996ffb0e6b916959abf9870392aee530ead
windowsnipz <nstrickland2014@gmail.com>
2024-07-07 18:00:07 -0400

    having trouble withe the styling while adding Quote componenent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Quote from './Quote';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <Quote />
     </div>
   );
 }
---
+++ src/Quote.js
@@ -0,0 +1,25 @@
+import { Component } from "react"
+
+class Quote extends Component {
+
+    state = {
+        quote: '',
+        author: ''
+    }
+
+
+
+    render() {
+        return (
+            <div class="section">
+            <h1>Generate a random quote from Kanye West!</h1>
+            <div id="input-wrapper">
+                  <button id="quote-btn">What's up Kanye?</button>
+            </div>
+        </div>
+        );
+    }
+}
+
+
+export default Quote;
