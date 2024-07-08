30da244b6b4f4493b7ad8af2ef7a8172750ba1c5
windowsnipz <nstrickland2014@gmail.com>
2024-07-08 17:43:30 -0400

    Added footer as a component, but it is not fixed to bottom of the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import Quote from './Quote';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Quote />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p>
+              My mission is to make the world a better place.
+              Thank you for visiting my page.
+            </p>
+          </footer>
+    );
+}
+
+export default Footer;
