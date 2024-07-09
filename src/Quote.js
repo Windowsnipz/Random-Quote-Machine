781c9c9e40cc0eaa4886f9e835c9ff55250e6b78
windowsnipz <nstrickland2014@gmail.com>
2024-07-08 21:11:10 -0400

    lots of progress on the function. can pretty much get random quotes now. Now, I just need to work on executing function on page load

--- src/Quote.js
+++ src/Quote.js
@@ -3,12 +3,32 @@ import { Component } from "react"
 class Quote extends Component {
 
     state = {
-        quote: 'Quote text here :)',
-        author: 'Noah Strickland'
+        quote: '',
+        author: ''
     }
 
-    getQuote() {
+    getQuote = () => {
         
+        fetch("https://type.fit/api/quotes")
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                const randomIndex = Math.floor(Math.random() * data.length); // get random index in the quote data
+                const chosenQuote = data[randomIndex].text;
+                let chosenAuthor = data[randomIndex].author;
+                if (chosenAuthor === 'type.fit') {
+                    chosenAuthor = 'unknown';
+                } else if (chosenAuthor) {
+                    chosenAuthor = chosenAuthor.replace(', type.fit', ''); // replaces the wonky API string
+                }
+
+                this.setState({
+                    quote: chosenQuote,
+                    author: chosenAuthor
+                })
+
+            })
+            .catch(error => console.log('Error: ' + error));
     }
 
 
@@ -16,7 +36,7 @@ class Quote extends Component {
     render() {
         return (
             <div className="quote-box">
-                <h1 id="text">{this.state.quote}</h1>
+                <h1 id="text">"{this.state.quote}"</h1>
                 <h5 id="author">- {this.state.author}</h5>
                 <div id="input-wrapper">
                     <button id="new-quote" className="btn" onClick={this.getQuote}>New Quote</button>
