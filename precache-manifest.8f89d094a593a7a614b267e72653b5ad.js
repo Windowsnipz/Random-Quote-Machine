71d56f5c4513cb01ec1e5763e5cb74342ba74bc8
windowsnipz <nstrickland2014@gmail.com>
2024-07-31 20:59:49 -0400

    Updates

---
+++ precache-manifest.8f89d094a593a7a614b267e72653b5ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e72c64e938bab7ef6f28cf73a9fe3f",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "5e50123ccd22b5417cb9",
+    "url": "/Random-Quote-Machine/static/css/main.7c9ced5c.chunk.css"
+  },
+  {
+    "revision": "07f1a969bf66d8305e51",
+    "url": "/Random-Quote-Machine/static/js/2.72df5c2f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Random-Quote-Machine/static/js/2.72df5c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eff21a743cbbb40299d",
+    "url": "/Random-Quote-Machine/static/js/3.dab5eadb.chunk.js"
+  },
+  {
+    "revision": "5e50123ccd22b5417cb9",
+    "url": "/Random-Quote-Machine/static/js/main.e596d0de.chunk.js"
+  },
+  {
+    "revision": "60c7066a1cd0ee7ba5ef",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.5e72a314.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Random-Quote-Machine/static/media/logo.06e73328.svg"
+  }
+]);
